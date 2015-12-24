85882c5cc80195ae5050756b56d6e6325351c451
Max Huttunen <max.huttunen@gmail.com>
2015-12-24 11:21:03 +0200

    edit readme

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # react-native-simple-modal
 A simple JavaScript modal component for React Native.
+
+## Installation
+`npm install react-native-simple-modal --save`
+
+## Usage
+
+### Properties and their default values
+
+```javascript
+
+<Modal
+	ref="modal"
+	overlayOpacity={0.75}
+	animationDuration={200}
+	animationTension={40}
+	modalDidOpen={() => undefined}
+	modalDidClose={() => undefined}>
+   ...
+</Modal>
+```
+
+### Methods
+
+```javascript
+// opens the modal
+this.refs.modal.open();
+
+// closes the modal
+this.refs.modal.close();
+
+// can be used to animate a translation of the modal along the Y-axis
+// useful for example when the modal has an text input and the modal needs to move up so that it's not hidden behind the keyboard
+this.refs.modal.animateOffset(number);
+```
---
+++ index.js
@@ -0,0 +1,119 @@
+'use strict';
+
+import React from 'react-native';
+
+const {
+   View,
+   Component,
+   StyleSheet,
+   TouchableOpacity,
+   Animated,
+   PropTypes
+} = React;
+
+class Modal extends Component {
+   constructor() {
+      super();
+
+      this.state = {
+         opacity: new Animated.Value(0),
+         scale: new Animated.Value(0.8),
+         offset: new Animated.Value(0)
+      };
+   }
+   setPhase(toValue) {
+      const {animationDuration, animationTension} = this.props;
+      Animated.timing(
+         this.state.opacity,
+         {
+            toValue,
+            duration: animationDuration
+         }
+      ).start();
+
+      Animated.spring(
+         this.state.scale,
+         {
+            toValue: toValue ? 1 : 0.8,
+            tension: animationTension
+         }
+      ).start();
+
+      setTimeout(() => {
+         if (toValue)
+            this.props.modalDidOpen();
+         else
+            this.props.modalDidClose();
+      }, animationDuration);
+   }
+   render() {
+      const {opacity, open, scale, offset} = this.state;
+      const {overlayOpacity} = this.props;
+      return (
+         <View
+         pointerEvents={open ? 'auto' : 'none'}
+         style={[styles.absolute, styles.container]}>
+            <TouchableOpacity
+            style={styles.absolute}
+            onPress={this.close.bind(this)}
+            activeOpacity={0.75}>
+               <Animated.View style={{flex: 1, opacity, backgroundColor: 'rgba(0, 0, 0, ' + overlayOpacity + ')'}} />
+            </TouchableOpacity>
+            <Animated.View
+               style={[
+                  this.props.style,
+                  {opacity, transform: [{scale}, {translateY: offset}]}
+               ]}>
+               {this.props.children}
+            </Animated.View>
+         </View>
+      );
+   }
+   open() {
+      this.setState({open: true});
+      this.setPhase(1);
+   }
+   close() {
+      this.setState({open: false});
+      this.setPhase(0);
+   }
+   animateOffset(offset) {
+      Animated.spring(
+         this.state.offset,
+         {toValue: offset}
+      ).start();
+   }
+}
+
+Modal.propTypes = {
+   overlayOpacity: PropTypes.number,
+   animationDuration: PropTypes.number,
+   animationTension: PropTypes.number,
+   modalDidOpen: PropTypes.func,
+   modalDidClose: PropTypes.func
+};
+
+Modal.defaultProps = {
+   overlayOpacity: 0.75,
+   animationDuration: 200,
+   animationTension: 40,
+   modalDidOpen: () => undefined,
+   modalDidClose: () => undefined
+};
+
+
+const styles = StyleSheet.create({
+   absolute: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: 'rgba(0, 0, 0, 0)'
+   },
+   container: {
+      justifyContent: 'center'
+   }
+});
+
+export default Modal;
